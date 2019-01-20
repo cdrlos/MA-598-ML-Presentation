b3ed4c2cde053d677488d274c501a7d9630788ec
Карлос әфәнде <cemiliosal@gmail.com>
2019-01-20 18:53:29 -0500

    Initial ShareLaTeX Import

---
+++ auto/Homework_Template.el
@@ -0,0 +1,31 @@
+(TeX-add-style-hook
+ "Homework_Template"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt" "letterpaper")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "preamble"
+    "pdftexcmds"
+    "fancyhdr")
+   (TeX-add-symbols
+    '("test" 2)
+    "myname"
+    "artT"
+    "artsub"
+    "hwno"
+    "due"))
+ :latex)
+
